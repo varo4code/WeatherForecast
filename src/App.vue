dfea4dfafe0a899c789153c652192d63ac2c822a
VaroCode <varo4code@gmail.com>
2025-02-01 23:53:34 +0100

    Init commit with basic layout

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <main>
+    <div class="w-screen h-screen grid grid-cols-4 p-8 bg-[url(./assets/backgrounds/thunderstorm.svg)] bg-cover">
+      <div class="absolute inset-0 bg-teal-900 opacity-50"></div>
+      <div class="col-span-3 grid grid-rows-3 relative z-10">
+        <mainInfo />
+
+        <!-- <div class="grid grid-cols-3 gap-4 row-span-2">
+          <div class="border">Indoor</div>
+          <div class="border">Barometer</div>
+          <div class="border">Moon Phase</div>
+          <div class="border col-span-3">Forecast 12h</div>
+        </div> -->
+      </div>
+      <div class="border relative z-10">Air conditions Sidebar</div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+@import "tailwindcss";
+</style>
+
+<script setup lang="ts">
+import mainInfo from './weather/components/mainInfo.vue';
+</script>
---
+++ src/weather/components/mainInfo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="px-5 ">
+
+        <div class="flex items-center">
+            <img src="../../assets/icons/location.svg" alt="Location" class="h-6">
+            <span class="text-lg">New York</span>
+        </div>
+        <div class="text-4xl mt-4">
+            Thunderstorm
+        </div>
+        <div class="text-[95px] mt-[-10px]">
+            26ºC
+        </div>
+        <div class="text-[15px] mt-[-20px]">
+            Sunday | 12 Dec 2024 - 11:43 PM
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+</script>
